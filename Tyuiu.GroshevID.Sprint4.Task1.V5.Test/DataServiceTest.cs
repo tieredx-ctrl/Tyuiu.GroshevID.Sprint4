213ea942679bfaaf4b48cefc651aa3ac66e1ab65
Илья Грошев <Tyuiu.GroshevID@outlook.com>
2025-11-13 17:20:40 +0500

    Task1

---
+++ Tyuiu.GroshevID.Sprint4.Task1.V5.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.GroshevID.Sprint4.Task1.V5.Lib;
+
+namespace Tyuiu.GroshevID.Sprint4.Task1.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 9, 6, 6, 9, 7, 8, 5, 9, 6, 6, 8, 6, 8, 6, 7 };
+
+            int res = ds.Calculate(array);
+            int wait = 60;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GroshevID.Sprint4.Task1.V5/Program.cs
@@ -0,0 +1,56 @@
+﻿using Tyuiu.GroshevID.Sprint4.Task1.V5.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнил: Грошев И. Д. | РППб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы. (ввод с клавиатуры)                           *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #5                                                              *");
+        Console.WriteLine("* Выполнил: Грошев Илья Дмитриевич | РППб-25-1                            *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 15 элементов заполненный         *");
+        Console.WriteLine("* значениями с клавиатуры в диапазоне от 5 до 9 подсчитать сумму четных   *");
+        Console.WriteLine("* элементов массива. С клавиатуры: 9, 6, 6, 9, 7, 8, 5, 9, 6, 6, 8, 6, 8, *");
+        Console.WriteLine("* 6, 7                                                                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int len;
+        Console.Write("Введите количество элементов массива: ");
+        len = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[len];
+
+        for (int i = 0; i < len; i++)
+        {
+            Console.Write("Введите значение " + i + " элемента массива: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < len; i++)
+        {
+            Console.Write(array[i] + "\t");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = ds.Calculate(array);
+
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
