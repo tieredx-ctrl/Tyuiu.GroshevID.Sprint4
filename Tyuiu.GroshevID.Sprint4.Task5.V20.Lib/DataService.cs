fcb5365e57f75845b474f7dd09df767b407fc7f7
Илья Грошев <Tyuiu.GroshevID@outlook.com>
2025-11-14 19:44:40 +0500

    Task5

---
+++ Tyuiu.GroshevID.Sprint4.Task5.V20.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.GroshevID.Sprint4.Task5.V20.Lib
+{
+    public class DataService : ISprint4Task5V20
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+            int res = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] == Math.Abs(matrix[i, j]))
+                    {
+                        res += matrix[i, j];
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.GroshevID.Sprint4.Task5.V20.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.GroshevID.Sprint4.Task5.V20.Lib;
+
+namespace Tyuiu.GroshevID.Sprint4.Task5.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[,]
+            {
+                { -4, -2, 0, -1, -3 },
+                { 3, 2, -1, 0, -1 },
+                { 0, 0, 1, 3, 3 },
+                { -7, -3, 3, 0, -1 },
+                { 0, 0, 0, -1, -7 }
+            };
+
+            int res = ds.Calculate(matrix);
+            int wait = 15;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
